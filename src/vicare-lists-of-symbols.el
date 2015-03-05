11c3ec766da5b98283227ca0e2e24de5a9577b45
Marco Maggi <marco.maggi-ipsu@poste.it>
2015-03-05 06:47:20 +0100

    removed font locking for load-r6rs-script which is no more a core primitive

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2015-03-01 11:23:30 marco>
+;; Time-stamp: <2015-03-05 06:46:56 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -1172,7 +1172,6 @@ language built ins.")
     "last-pair"
     "library"
     "load"
-    "load-r6rs-script"
     "lookahead-two-u8"
     "machine-word->integer"
     "make-binary-file-descriptor-input/output-port"
