d6ac309a34c50fd2dd65b9a3dd119a0644217b04
Marco Maggi <marco.maggi-ipsu@poste.it>
2015-03-12 12:58:44 +0100

    added core primitive record=?

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2015-03-12 09:06:24 marco>
+;; Time-stamp: <2015-03-12 12:39:59 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -1371,6 +1371,7 @@ language built ins.")
     "record-destructor-set!"
     "record-guardian-log"
     "record-guardian-logger"
+    "record=?"
     "record-reset"
     "record-object?"
     "record-and-rtd?"
