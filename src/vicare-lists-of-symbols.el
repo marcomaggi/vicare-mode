927231266bd16a1fc88512647c17cd26f82c5ec0
Marco Maggi <marco.maggi-ipsu@poste.it>
2015-03-21 08:31:18 +0100

    more core primitives

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2015-03-21 06:32:00 marco>
+;; Time-stamp: <2015-03-21 08:27:09 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -1505,6 +1505,11 @@ language built ins.")
     "symbol-bound?"
     "symbol->keyword"
     "symbol-value"
+    "symbol<?"
+    "symbol<=?"
+    "symbol>?"
+    "symbol>=?"
+    "list-of-symbols?"
     "syntax-object?"
     "syntax-object-expression"
     "syntax-object-marks"
