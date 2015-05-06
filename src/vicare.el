81a80744f9dcea8f2f3cc4b392b114c8195602b8
Marco Maggi <marco.maggi-ipsu@poste.it>
2015-05-06 10:03:33 +0200

    more indentation

--- src/vicare.el
+++ src/vicare.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2015-05-03 08:39:02 marco>
+;; Time-stamp: <2015-05-06 09:39:43 marco>
 ;; Keywords: languages
 
 ;; This file is part of Vicare Mode.
@@ -583,6 +583,7 @@ in the Scheme mode hook."
     (check-ec						. 2)
     (check-for-expression-return-value-violation	. 1)
     (check-for-procedure-argument-violation		. 1)
+    (check-for-procedure-signature-argument-violation	. 1)
     (check-for-procedure-argument-consistency-violation	. 1)
     (compensate						. vicare-indent-compensate)
     (coroutine						. 1)
@@ -699,6 +700,7 @@ in the Scheme mode hook."
     (open-file-input-port				. 1)
     (raise-non-continuable-standard-condition		. 1)
     (procedure-argument-violation			. 1)
+    (procedure-signature-argument-violation		. 1)
     (procedure-arguments-consistency-violation		. 1)
     (r6.make-token-lexer				. 1)
     (rec						. 1)
