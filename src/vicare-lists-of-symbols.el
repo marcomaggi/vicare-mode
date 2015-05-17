9690f2faf00d5238cd50f57fadd093e8966b6ba7
Marco Maggi <marco.maggi-ipsu@poste.it>
2015-05-17 10:31:47 +0200

    more font locking

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2015-05-17 10:06:06 marco>
+;; Time-stamp: <2015-05-17 10:31:38 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -788,7 +788,7 @@ language built ins.")
 
     "expander-options" "compiler-options"
 
-    "type-of" "expansion-of" "visit-code-of"
+    "type-of" "expansion-of" "expansion-of*" "visit-code-of"
     "optimisation-of" "optimisation-of*"
     "further-optimisation-of" "further-optimisation-of*"
     "assembly-of"
