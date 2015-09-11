6f48c9bad55752ce9e7457822e6b0f8e9c17e21d
Marco Maggi <marco.maggi-ipsu@poste.it>
2015-09-11 15:24:39 +0200

    added support for syntax "method-call"

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2015-09-10 09:30:25 marco>
+;; Time-stamp: <2015-09-11 15:23:06 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -822,7 +822,7 @@ language built ins.")
 
     "lambda*" "case-lambda*"
 
-    "type-descriptor" "is-a?" "slot-set!" "slot-ref"
+    "type-descriptor" "is-a?" "slot-set!" "slot-ref" "method-call"
     "condition-is-a?"
 
     "tag-predicate"
--- src/vicare.el
+++ src/vicare.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2015-09-10 09:30:39 marco>
+;; Time-stamp: <2015-09-11 15:24:21 marco>
 ;; Keywords: languages
 
 ;; This file is part of Vicare Mode.
@@ -695,6 +695,7 @@ in the Scheme mode hook."
     (match-define*					. 1)
     (match-let						. 1)
     (match-let*						. 1)
+    (method-call					. 1)
     (mk.define-maker					. 2)
     (unwinding-call/cc					. 1)
 
