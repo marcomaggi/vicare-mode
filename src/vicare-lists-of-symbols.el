1a303e900d57ce0d51d7ba08a74d980bf4dc200f
Marco Maggi <marco.maggi-ipsu@poste.it>
2015-10-12 16:02:07 +0200

    renamed syntaxes

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2015-10-10 07:42:19 marco>
+;; Time-stamp: <2015-10-12 16:01:06 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -829,8 +829,8 @@ language built ins.")
     "unsafe-cast" "new" "delete"
     "validate-typed-procedure-argument"
     "validate-typed-return-value"
-    "assert-retvals-signature"
-    "assert-retvals-signature-and-return"
+    "assert-signature"
+    "assert-signature-and-return"
     "type-super-and-sub?"
 
     "make-parameter" "parametrise" "parameterise" "parameterize"
