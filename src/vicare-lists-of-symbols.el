62ad4db7f78ac03fbdf0ee25c2fd34262b871a36
Marco Maggi <marco.maggi-ipsu@poste.it>
2016-04-04 12:48:16 +0200

    updated font locking for built-in syntaxes

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2016-03-28 15:38:01 marco>
+;; Time-stamp: <2016-04-04 12:29:54 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -885,7 +885,7 @@ language built ins.")
     "brace" "return" "continue" "break" "with"
     "visit-upon-loading" "typed-language" "strict-r6rs"
     "stdin" "stdout" "stderr"
-    "pair" "pair-of" "list-of" "vector-of" "union")
+    "pair" "pair-of" "list-of" "vector-of" "hashtable" "alist")
   "List of auxiliary syntaxes from the Vicare libraries.")
 
 (defconst vicare-vicare-functions-list
