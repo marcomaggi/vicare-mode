d097c4e00eb09c8fbf5049af19e73de9e5905d96
Marco Maggi <marco.maggi-ipsu@poste.it>
2016-04-19 20:39:02 +0200

    updated support for built-in syntaxes

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2016-04-18 11:32:17 marco>
+;; Time-stamp: <2016-04-19 20:38:37 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -891,7 +891,7 @@ language built ins.")
     "visit-upon-loading" "typed-language" "strict-r6rs"
     "stdin" "stdout" "stderr"
     "pair" "pair-of" "list-of" "vector-of" "hashtable" "alist"
-    "parent-of" "ancestors-of")
+    "parent-of" "ancestor-of")
   "List of auxiliary syntaxes from the Vicare libraries.")
 
 (defconst vicare-vicare-functions-list
