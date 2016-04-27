b21059482653c45bb238d3b83c305bb3f55bf774
Marco Maggi <marco.maggi-ipsu@poste.it>
2016-04-27 10:14:14 +0200

    updated support for built-in syntaxes

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2016-04-27 06:44:26 marco>
+;; Time-stamp: <2016-04-27 09:46:37 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -840,8 +840,8 @@ language built ins.")
     "assert-signature" "assert-signature-and-return"
     "cast-signature" "unsafe-cast-signature"
     "type-of" "type-annotation=?"
-    "type-annotation-super-and-sub?" "type-annotation-common-ancestor" "type-annotation-ancestors" "type-annotation-union"
-    "type-annotation-matching"
+    "type-annotation-super-and-sub?" "type-annotation-common-ancestor" "type-annotation-ancestors"
+    "type-annotation-syntax" "type-annotation-matching"
     "type-signature-super-and-sub?" "type-signature-matching" "type-signature-union"  "type-signature-common-ancestor"
 
     "make-parameter" "parametrise" "parameterise" "parameterize"
