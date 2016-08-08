e048ea39779fd787436b4b802fc4f5a1e6c391f1
Marco Maggi <marco.maggi-ipsu@poste.it>
2016-08-08 07:35:04 +0200

    updated support for built-in syntaxes

--- src/vicare-lists-of-symbols.el
+++ src/vicare-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <marco.maggi-ipsu@poste.it>
 ;; Created: Tue Dec 10, 2013
-;; Time-stamp: <2016-07-29 12:38:51 marco>
+;; Time-stamp: <2016-08-08 07:25:41 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Home Directory.
@@ -906,7 +906,7 @@ language built ins.")
     "?" ":" "!" "%" "&" "^" "~" "<<" ">>" "⏐" "⏐⏐" "¦" "¦¦" "**" "=="
     "∧" "∨" "⊻" "¬" "×" "≠" "⋅"
     "brace" "return" "continue" "break" "with"
-    "default-language" "typed-language" "strict-r6rs"
+    "default-language" "typed-language" "strict-r6rs" "strict-type-checking"
     "stdin" "stdout" "stderr"
     "pair" "pair-of" "list-of" "nelist-of" "vector-of" "nevector-of" "hashtable" "alist" "enumeration"
     "parent-of" "ancestor-of"
